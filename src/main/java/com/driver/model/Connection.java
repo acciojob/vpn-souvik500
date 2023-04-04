ddba6544c0c984ecdd6832c0fdc2065001ec9361
souvikadhikary <souvik500>
2023-04-04 13:28:49 +0530

    error

--- src/main/java/com/driver/model/Connection.java
+++ src/main/java/com/driver/model/Connection.java
@@ -1,28 +1,50 @@
 package com.driver.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-@Builder
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
-public class Connection
-{
+@Table(name = "connections")
+public class Connection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     @ManyToOne
     @JoinColumn
-    User user;
-
+    private ServiceProvider serviceProvider;
     @ManyToOne
     @JoinColumn
-    ServiceProvider serviceProvider;
+    private User user;
+
+    public Connection() {
+    }
+
+    public Connection(int id, ServiceProvider serviceProvider, User user) {
+        this.id = id;
+        this.serviceProvider = serviceProvider;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ServiceProvider getServiceProvider() {
+        return serviceProvider;
+    }
+
+    public void setServiceProvider(ServiceProvider serviceProvider) {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
